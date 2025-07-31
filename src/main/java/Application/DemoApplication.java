093f82b26514e58aa6a7e6d88731d004c5c7f188
khoanguyen2108 <2251120021@ut.edu.vn>
2025-07-31 22:30:26 +0700

    feat: update architecture for java api

---
+++ src/main/java/Application/DemoApplication.java
@@ -0,0 +1,22 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/api/hello")
+        public String sayHello() {
+            return "Hello from Spring Boot!";
+        }
+    }
+}
