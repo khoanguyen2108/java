745edebdb965da98a26ce574d1e00e053b6f31c5
khoanguyen2108 <2251120021@ut.edu.vn>
2025-08-07 00:07:42 +0700

    feat: fix and create new source
    done

--- src/test/java/com/example/pod_booking_system/PodBookingSystemApplicationTests.java
+++ src/test/java/com/example/pod_booking_system/PodBookingSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.pod_booking_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PodBookingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
